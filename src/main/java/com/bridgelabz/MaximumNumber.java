68673883f4d0a8c73f59866604052ebe937d2cd0
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 10:35:21 -0500

    Add :Max Number at 1st Position for float

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -2,10 +2,17 @@ package com.bridgelabz;
 
 public class MaximumNumber {
     Integer d;
+    Float e;
 
     public Integer getMaxNumber(Integer a, Integer b, Integer c) {
         d = (a.toString().compareTo(b.toString()) > 0 ? (a.toString().compareTo(c.toString()) > 0 ? a : c)
                 : (b.toString().compareTo(c.toString()) > 0 ? b : c));
         return d;
+    }
+
+    public Float getMaxNumber(Float a, Float b, Float c) {
+        e = (a.toString().compareTo(b.toString()) > 0 ? (a.toString().compareTo(c.toString()) > 0 ? a : c)
+                : (b.toString().compareTo(c.toString()) > 0 ? b : c));
+        return e;
     }
 }
--- src/test/java/com/bridgelabz/MaxNumberTest.java
+++ src/test/java/com/bridgelabz/MaxNumberTest.java
@@ -2,26 +2,35 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.omg.PortableInterceptor.INACTIVE;
 
 public class MaxNumberTest {
     @Test
-    public void givenMaxNumberAt_1stPosition_ShouldReturn_theSameNumber() {
+    public void givenMaxNumberAt_1stPosition_ShouldReturn_theSameNumberForInteger() {
         MaximumNumber m1 = new MaximumNumber();
-        Object result = m1.getMaxNumber(40, 20, 30);
+        Integer result = m1.getMaxNumber(40, 20, 30);
         Assert.assertEquals(Integer.valueOf(40), result);
     }
 
     @Test
-    public void givenMaxNumberAt_2ndPosition_ShouldReturn_theSameNumber() {
+    public void givenMaxNumberAt_2ndPosition_ShouldReturn_theSameNumberForInteger() {
         MaximumNumber m1 = new MaximumNumber();
-        Object result = m1.getMaxNumber(10, 40, 30);
+        Integer result = m1.getMaxNumber(10, 40, 30);
         Assert.assertEquals(Integer.valueOf(40), result);
     }
 
     @Test
-    public void givenMaxNumberAt_3rd_Position_ShouldReturn_theSameNumber() {
+    public void givenMaxNumberAt_3rd_Position_ShouldReturn_theSameNumberForInteger() {
         MaximumNumber m1 = new MaximumNumber();
-        Object result = m1.getMaxNumber(10, 20, 30);
+        Integer result = m1.getMaxNumber(10, 20, 30);
         Assert.assertEquals(Integer.valueOf(30), result);
     }
+
+    @Test
+    public void givenMaxNumberAt_1stPosition_ShouldReturn_theSameNumberForFloat() {
+        MaximumNumber m1 = new MaximumNumber();
+        Float result = m1.getMaxNumber(40.10f, 20.10f, 30.10f);
+        Assert.assertEquals(Float.valueOf(40.10f), result);
+    }
+
 }
