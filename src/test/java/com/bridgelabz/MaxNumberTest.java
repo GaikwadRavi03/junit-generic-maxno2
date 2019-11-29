0848d6a4cd157159029dac873552929071d02b59
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 10:22:42 -0500

    Add :Max Number at 2st Position

--- src/test/java/com/bridgelabz/MaxNumberTest.java
+++ src/test/java/com/bridgelabz/MaxNumberTest.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 
 public class MaxNumberTest {
     @Test
-    public void givenMaxNumberat_1stPosition_ShouldReturn_theSameNumber() {
+    public void givenMaxNumberAt_1stPosition_ShouldReturn_theSameNumber() {
         MaximumNumber m1 = new MaximumNumber();
         Object result = m1.getMaxNumber(40, 20, 30);
+        Assert.assertEquals(Integer.valueOf(40), result);
+    }
+    @Test
+    public void givenMaxNumberAt_2stPosition_ShouldReturn_theSameNumber() {
+        MaximumNumber m1 = new MaximumNumber();
+        Object result = m1.getMaxNumber(10, 40, 30);
         Assert.assertEquals(Integer.valueOf(40), result);
     }
 }
