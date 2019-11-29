7bb299eedae8c970c3ab3284966680515b212151
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 11:07:23 -0500

    Refactor1: All the 3 Methods to in One Generic Method and find the maximum one.

--- src/main/java/com/bridgelabz/MaximumNumber.java
+++ src/main/java/com/bridgelabz/MaximumNumber.java
@@ -1,24 +1,11 @@
 package com.bridgelabz;
 
-public class MaximumNumber {
-    Integer d;
-    Float e;
-    String f;
+public class MaximumNumber<T> {
+    T d;
 
-    public Integer getMaxNumber(Integer a, Integer b, Integer c) {
-        d = (a.toString().compareTo(b.toString()) > 0 ? (a.toString().compareTo(c.toString()) > 0 ? a : c)
+    public static <T> T getMaxNumber(T a, T b, T c) {
+        T d = (a.toString().compareTo(b.toString()) > 0 ? (a.toString().compareTo(c.toString()) > 0 ? a : c)
                 : (b.toString().compareTo(c.toString()) > 0 ? b : c));
         return d;
-    }
-
-    public Float getMaxNumber(Float a, Float b, Float c) {
-        e = (a.toString().compareTo(b.toString()) > 0 ? (a.toString().compareTo(c.toString()) > 0 ? a : c)
-                : (b.toString().compareTo(c.toString()) > 0 ? b : c));
-        return e;
-    }
-
-    public String getMaxNumber(String a, String b, String c) {
-        f = (a.compareTo(b) > 0 ? (a.compareTo(c) > 0 ? a : c) : (b.compareTo(c) > 0 ? b : c));
-        return f;
     }
 }
