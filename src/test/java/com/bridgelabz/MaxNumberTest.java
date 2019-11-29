1300080d5f701781d88d7e97f7590b9b3b034770
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-29 10:24:37 -0500

    Add :Max Number at 3rd Position

--- src/test/java/com/bridgelabz/MaxNumberTest.java
+++ src/test/java/com/bridgelabz/MaxNumberTest.java
@@ -10,10 +10,18 @@ public class MaxNumberTest {
         Object result = m1.getMaxNumber(40, 20, 30);
         Assert.assertEquals(Integer.valueOf(40), result);
     }
+
     @Test
-    public void givenMaxNumberAt_2stPosition_ShouldReturn_theSameNumber() {
+    public void givenMaxNumberAt_2ndPosition_ShouldReturn_theSameNumber() {
         MaximumNumber m1 = new MaximumNumber();
         Object result = m1.getMaxNumber(10, 40, 30);
         Assert.assertEquals(Integer.valueOf(40), result);
+    }
+
+    @Test
+    public void givenMaxNumberAt_3rd_Position_ShouldReturn_theSameNumber() {
+        MaximumNumber m1 = new MaximumNumber();
+        Object result = m1.getMaxNumber(10, 20, 30);
+        Assert.assertEquals(Integer.valueOf(30), result);
     }
 }
